87c4b58d01b480191c60b25decc04b2574e4bd95
Facundo Barrios <cf22facundo.barrios@iesjoandaustria.org>
2023-11-19 21:25:49 +0100

    ahora si final final

--- Barrios Facundo P1/Practica 1/app/Http/Controllers/SignController.php
+++ Barrios Facundo P1/Practica 1/app/Http/Controllers/SignController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FirstController extends Controller
+class SignController extends Controller
 {
     public function signin($v1, $v2, $v3, $v4) {  //params pasados en la url
         return view('signin')->with(['v1' => $v1, 'v2' => $v2, 'v3' => $v3, 'v4' => $v4]);
--- Barrios Facundo P1/Practica 1/routes/web.php
+++ Barrios Facundo P1/Practica 1/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FirstController;
+use App\Http\Controllers\SignController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,8 @@ Route::get('/', function () {
 });
 
 Route::prefix('facundo')->group(function(){
-    //Route::get('/signin', [FirstController::class, 'signin']);    // sin param ya no funcionan
-    //Route::get('/signup', [FirstController::class, 'signup']);    // sin param ya no funcionan
-    Route::get('/signin/{v1}/{v2}/{v3}/{v4}', [FirstController::class, 'signin']);
-    Route::get('/signup/{v1}/{v2}/{v3}', [FirstController::class, 'signup']);
+    //Route::get('/signin', [SignController::class, 'signin']);    // sin param ya no funcionan
+    //Route::get('/signup', [SignController::class, 'signup']);    // sin param ya no funcionan
+    Route::get('/signin/{v1}/{v2}/{v3}/{v4}', [SignController::class, 'signin']);
+    Route::get('/signup/{v1}/{v2}/{v3}', [SignController::class, 'signup']);
 });
