ee169a8620ff5fec5c90f7661c2a4c66feacde88
Facundo Barrios <cf22facundo.barrios@iesjoandaustria.org>
2023-12-13 17:08:25 +0100

    cambio a p4 grupal

--- Barrios Facundo P3/Practica 3/storage/framework/views/4f49833e325c961428f5d34e7014fc81.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/facundo/Documents/daw2/m07/M07_UF2/Barrios Facundo P2/Practica 2/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
