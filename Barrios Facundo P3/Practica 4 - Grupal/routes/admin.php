da096c2f21d3930681a8feb7520a45a51f6cb9ed
angelusMZ19 <115651373+angelusMZ19@users.noreply.github.com>
2023-12-17 16:29:37 +0100

    practica4-alumnat

--- Barrios Facundo P3/Practica 4 - Grupal/routes/admin.php
+++ Barrios Facundo P3/Practica 4 - Grupal/routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\CentresController;
+use App\Http\Controllers\Admin\AlumnatController;//ruta de acceso al controlado de los alumnos 
 use App\Http\Controllers\Admin\ProfessoratController;
 
 
@@ -10,7 +11,13 @@ use App\Http\Controllers\Admin\ProfessoratController;
         Route::get('/admin', [AdminController::class, 'adminView'])->name('admin');
         Route::post('/login', [AdminController::class, 'login'])->name('login');
         Route::get('/centres', [CentresController::class, 'index'])->name('centres');
-        Route::get('/alumnat', [AdminController::class, 'alumnat'])->name('alumnat');
+        Route::get('/alumnat', [AlumnatController::class, 'index'])->name('indexAlumnat');//Listar todos los alumnos
+        Route::get('/a_create', [AlumnatController::class, 'create'])->name('createAlumnat');//Formulario para crear un nuevo alumno
+        Route::post('/a_store', [AlumnatController::class, 'store'])->name('storeAlumnat');//Almacenar un nuevo alumno en la base de datos
+        Route::get('/a_show', [AlumnatController::class, 'show'])->name('showAlumnat');//Mostrar detalles de un alumno específico
+        Route::get('/a_edit/{id}', [AlumnatController::class, 'edit'])->name('editAlumnat');//Formulario para editar un alumno específico
+        Route::get('/a_update/{id}', [AlumnatController::class, 'update'])->name('updateAlumnat');//Actualizar un alumno en la base de datos
+        Route::delete('/a_destroy/{alumnat}', [AlumnatController::class, 'destroy'])->name('deleteAlumnat');//Eliminar un alumno de BBDD
         Route::get('/professorat', [ProfessoratController::class, 'index'])->name('professorat');
         Route::get('/p_create', [ProfessoratController::class, 'create'])->name('p_create');
         Route::get('/p_store', [ProfessoratController::class, 'store'])->name('p_store');
